34298ceb6c3519ce3c1b391b6b421f7bd1a422b4
Laknidu Thilakawardhana <10899430@students.plymouth.ac.uk>
2025-03-01 12:34:44 +0530

    changes

---
+++ Inventory/inventory-backend/.sequelizerc.cjs
@@ -0,0 +1,9 @@
+// // .sequelizerc (MUST be CommonJS)
+// const path = require('path');
+
+// module.exports = {
+//   config: path.resolve('config', 'config.cjs'), // Path to your JS/CJS config
+//   'models-path': path.resolve('models'),
+//   // 'migrations-path': path.resolve('migrations'),
+//   'seeders-path': path.resolve('seeders')
+// };
